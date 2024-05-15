f4c0b18dfa3df2d927258ed8a2424c8a8e118cd3
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-05-14 21:43:32 -0500

    feat: Refactorizar funciones de Usuario.ts y agregar comentarios

--- src/data/Usuario.ts
+++ src/data/Usuario.ts
@@ -3,7 +3,7 @@ import { Usuario } from "@/types/model";
 
 export async function ObtenerUsuario() {
   const result = await axios.get("http://localhost:3000/api/usuario");
-  console.log(result.data);
+  //console.log(result.data);
   return result.data;
 }
 
@@ -11,21 +11,24 @@ export async function CrearUsuario(
   usuario: Omit<Usuario, "id_usuario" | "puntos">
 ) {
   const result = await axios.post("http://localhost:3000/api/usuario", usuario);
-  console.log(result.data);
+  //console.log(result.data);
   return result.data;
 }
 
-export async function ActualizarUsuario(usuario: Usuario, id: number) {
+export async function ActualizarUsuario(
+  usuario: Omit<Usuario, "id_usuario">,
+  id: number
+) {
   const result = await axios.put(
     `http://localhost:3000/api/usuario/${id}`,
     usuario
   );
-  console.log(result.data);
+  //console.log(result.data);
   return result.data;
 }
 
 export async function EliminarUsuario(id: number) {
   const result = await axios.delete(`http://localhost:3000/api/usuario/${id}`);
-  console.log(result.data);
+  //console.log(result.data);
   return result.data;
 }
