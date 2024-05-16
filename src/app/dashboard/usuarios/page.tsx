a79e178aa47ebc962fe113a8b62f6d4e419682dc
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-05-15 20:24:50 -0500

    feat: Add dashboard usuarios page and promociones page

---
+++ src/app/dashboard/usuarios/page.tsx
@@ -0,0 +1,9 @@
+import SectionUsuario from "./_components";
+
+export default function Page() {
+  return (
+    <div className='container mx-auto py-10'>
+      <SectionUsuario />
+    </div>
+  );
+}
