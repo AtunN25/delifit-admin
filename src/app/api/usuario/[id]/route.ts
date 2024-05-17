37d914a0459f4cc879f29f5edad7ed5cc8d1e203
epaniaguam <eypaniagua@unjbg.edu.pe>
2024-05-16 21:35:18 -0500

    feat: Agregar endpoints para obtener, eliminar, actualizar y parchear usuarios

--- src/app/api/usuario/[id]/route.ts
+++ src/app/api/usuario/[id]/route.ts
@@ -5,19 +5,40 @@ interface Params {
   id: string;
 }
 
-// DELETE es el metodo para eliminar un usuario
-export async function DELETE(request: Request, context: { params: Params }) {
-  const result = await sql`
-    DELETE FROM usuario
-    WHERE id = ${context.params.id}
-    RETURNING *`;
-  return NextResponse.json({ data: result }, { status: 200 });
+export async function GET(request: Request, context: { params: Params }) {
+  const { id } = context.params;
+  const [dataDB] =
+    await sql`SELECT * FROM usuario where usuario.id_usuario = ${id}`;
+
+  // En caso el usuario no exista
+  if (!dataDB || dataDB.length === 0) {
+    return NextResponse.json(
+      { message: "Usuario no encontrado" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json({ data: dataDB }, { status: 200 });
 }
 
 // PUT es el metodo para actualizar un usuario
 export async function PUT(request: Request, context: { params: Params }) {
+  const { id } = context.params;
+
+  let [dataDB] =
+    await sql`SELECT * FROM usuario where usuario.id_usuario = ${id}`;
+
+  // En caso el usuario no exista
+  if (!dataDB || dataDB.length === 0) {
+    return NextResponse.json(
+      { message: "Usuario no encontrado" },
+      { status: 404 }
+    );
+  }
+
   const { nombre, apellido, img_url, telefono, documento, tipo_doc, puntos } =
     await request.json();
+
   const result = await sql`
     UPDATE usuario 
     SET nombre = ${nombre}, 
@@ -29,5 +50,63 @@ export async function PUT(request: Request, context: { params: Params }) {
         puntos = ${puntos}
     WHERE id = ${context.params.id}
     RETURNING *`;
+  return NextResponse.json({ data: result }, { status: 200 });
+}
+
+// PATCH es el metodo para actualizar un usuario
+
+export async function PATCH(request: Request, context: { params: Params }) {
+  const { id } = context.params;
+
+  const [result] =
+    await sql`SELECT * FROM usuario where usuario.id_usuario = ${id}`;
+
+  // En caso el usuario no exista
+  if (!result || result.length === 0) {
+    return NextResponse.json(
+      { message: "Usuario no encontrado" },
+      { status: 404 }
+    );
+  }
+
+  const data = await request.json();
+
+  // Actualizo el usuario
+  const userUpd = { ...result, ...data };
+
+  console.log(userUpd);
+
+  await sql`
+    UPDATE usuario 
+    SET 
+      img_url = ${userUpd.img_url}, 
+      nombre = ${userUpd.nombre}, 
+      apellido = ${userUpd.apellido}, 
+      puntos = ${userUpd.puntos},
+      tipo_doc = ${userUpd.tipo_doc},
+      telefono = ${userUpd.telefono},
+      documento = ${userUpd.documento}
+    WHERE id_usuario = ${id}
+    RETURNING *
+  `;
+  return NextResponse.json({ data: userUpd }, { status: 200 });
+}
+
+// DELETE es el metodo para eliminar un usuario
+export async function DELETE(request: Request, context: { params: Params }) {
+  const { id } = context.params;
+
+  const [dataDB] =
+    await sql`SELECT * FROM usuario where usuario.id_usuario = ${id}`;
+
+  if (!dataDB || dataDB.length === 0) {
+    return NextResponse.json(
+      { message: "Usuario no encontrado" },
+      { status: 404 }
+    );
+  }
+
+  const result = await sql`
+      DELETE FROM usuario WHERE usuario.id_usuario = ${id} RETURNING *`;
   return NextResponse.json({ data: result }, { status: 200 });
 }
--- src/app/api/usuario/route.ts
+++ src/app/api/usuario/route.ts
@@ -4,17 +4,23 @@ import { NextResponse } from "next/server";
 // GET es el metodo para obtener todos los usuarios
 export async function GET(request: Request) {
   const result = await sql`SELECT * FROM usuario`;
-  //console.log(result);
+  // console.log(result);
   return NextResponse.json({ data: result }, { status: 200 });
 }
 
 // POST es el metodo para crear un usuario
 export async function POST(request: Request) {
-  const { nombre, apellido, img_url, telefono, documento, tipo_doc } =
+  const { nombre, apellido, img_url, telefono, documento, puntos, tipo_doc } =
     await request.json();
   const result = await sql`
-    INSERT INTO usuario (nombre, apellido, img_url, telefono, documento, tipo_doc)
-    VALUES (${nombre}, ${apellido}, ${img_url}, ${telefono}, ${documento}, ${tipo_doc})
-    RETURNING *`;
+    SELECT public.registrar_usuario(
+      ${img_url},
+      ${nombre}, 
+      ${apellido}, 
+      ${documento}, 
+      ${puntos}, 
+      ${tipo_doc},
+      ${telefono}
+    )`;
   return NextResponse.json({ data: result }, { status: 201 });
 }
