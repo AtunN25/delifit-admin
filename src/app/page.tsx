f38892b200c32f7ff8518c35875b51ab2e7a9a62
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-05-25 21:23:54 -0500

    feat: Actualizar Icono, título y descripción en metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   console.log("Home");
   return (
@@ -5,6 +7,14 @@ export default function Home() {
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         Pagina Principal
       </div>
+      <h1>Queque</h1>
+      <Image
+        src='/producto/queque.jpg'
+        alt='Queque'
+        width={500}
+        height={500}
+        className='rounded'
+      />
     </main>
   );
 }
